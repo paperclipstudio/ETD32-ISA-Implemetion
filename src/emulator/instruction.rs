dc754a6965500ba56373848892645338f599a4ae
George Sanger <george.sanger@consult.red>
2023-10-14 15:50:35 +0100

    instruction encoding tests

--- src/emulator/instruction.rs
+++ src/emulator/instruction.rs
@@ -26,13 +26,21 @@ impl Instruction {
     // TODO Add tests?
     // TODO Add checks for oversized values
     // TODO Must be a nicer way to do this....
-        (if self.carry {1<< 31} else{ return 0 }) as u32 |
-        (if self.less_than_zero {1<< 30} else{ return 0 }) as u32 |
-        (if self.equal_to_zero {1<< 29} else{ return 0 }) as u32|
-        (if self.greater_than_zero {1<< 28} else{ return 0 }) as u32 |
+        if self.opcode > 0x3F {
+            panic!("opcode too large");
+        };
+        // TODO move this earlier
+        if self.operands > 0x1FFFFF {
+            panic!("opcode too large");
+        };
+        let num = (if self.carry             {(1 as u32) << 31} else{ 0 }) |
+        (if self.less_than_zero    {1<< 30} else{ 0 }) as u32 |
+        (if self.equal_to_zero     {1<< 29} else{ 0 }) as u32 |
+        (if self.greater_than_zero {1<< 28} else{ 0 }) as u32 |
         // TODO have way to make sure this doesn't overflow
-        (self.opcode            << 22) as u32 |
-        self.operands          <<  0
+        (self.opcode as u32)            << 22 |
+        self.operands;
+        return num;
     }
 
     pub fn decode(value: u32) -> Self {
@@ -54,8 +62,44 @@ mod tests {
     use super::*;
 
     #[test]
-    fn fail() {
-        assert!(false)
+    fn encode_interupt() {
+        let instruction = Instruction::from_opcode(32, 0xABC);
+        let target = 0x0800_0ABC;
+        let result = instruction.encode();
+        assert_eq!(target, result, "\nT: {target:08x}\nR: {result:08x}");
+    }
+    
+    #[test]
+    fn encode_flags() {
+        let mut instruction = Instruction::from_opcode(32, 0xABC);
+        instruction.carry = true;
+        let target = 0x8800_0ABC;
+        let result = instruction.encode();
+        assert_eq!(target, result, "\nT: {target:08x}\nR: {result:08x}");
+
+        instruction.less_than_zero = true;
+        let target = 0xC800_0ABC;
+        let result = instruction.encode();
+        assert_eq!(target, result, "\nT: {target:08x}\nR: {result:08x}");
+
+        instruction.equal_to_zero = true;
+        let target = 0xE800_0ABC;
+        let result = instruction.encode();
+        assert_eq!(target, result, "\nT: {target:08x}\nR: {result:08x}");
+
+        instruction.greater_than_zero = true;
+        let target = 0xF800_0ABC;
+        let result = instruction.encode();
+        assert_eq!(target, result, "\nT: {target:08x}\nR: {result:08x}");
+
+    }
+
+    #[test]
+    fn encode_jump_offset() {
+        let instruction = Instruction::from_opcode(31, 0xDCA);
+        let target = 0x07C0_0DCA;
+        let result = instruction.encode();
+        assert_eq!(target, result, "\nT: {target:08x}\nR: {result:08x}");
     }
 }
 
