fd2f7f44ef92a2857c150cb9665bc8e590944952
George Sanger <paperclipstudio@gmail.com>
2023-10-28 12:51:50 +0100

    Store 16 bo Done

--- src/emulator.rs
+++ src/emulator.rs
@@ -175,6 +175,7 @@ impl Cpu {
             22 => InstSet::load_32_bi(self),
             23 => InstSet::store_8_bo(self),
             24 => InstSet::store_8_bi(self),
+            25 => InstSet::store_16_bo(self),
             29 => InstSet::jump_offset(self),
             30 => InstSet::jump_to_rd(self),
             31 => InstSet::jump_to_i(self),
@@ -281,7 +282,6 @@ impl InstSet {
         let base = cpu.read(instruction.r_base());
         let memory_address = base + instruction.i_offset() as u8;
         println!("STORE|FROM:{}, TO:{}", instruction.r_target(), memory_address);
-        //TODO Add a check that this can all be done before hand. ie make atomic
         match cpu.copy_to_memory(instruction.r_target(), memory_address) {
             Ok(()) => (),
             Err(_msg) => return UnknownCpu::Inter(InterruptedCpu{cpu}),
@@ -295,10 +295,39 @@ impl InstSet {
         let offset = cpu.read(instruction.r_index());
         let memory_address = base + offset;
         println!("STORE|FROM:{}, TO:{}", instruction.r_target(), memory_address);
-        //TODO Add a check that this can all be done before hand. ie make atomic
         match cpu.copy_to_memory(instruction.r_target(), memory_address) {
             Ok(()) => (),
             Err(_msg) => return UnknownCpu::Inter(InterruptedCpu{cpu}),
+        }
+        UnknownCpu::Ok(cpu)
+    }
+
+    fn store_16_bo(mut cpu: Cpu) -> UnknownCpu {
+        let instruction = cpu.current_instruction();
+        let base = cpu.read(instruction.r_base());
+        let memory_address = base + instruction.i_offset() as u8;
+        println!("STORE|FROM:{}, TO:{}", instruction.r_target(), memory_address);
+        //TODO Make atomic
+        for i in 0..2 {
+            match cpu.copy_to_memory(instruction.r_target() + i, memory_address + i) {
+                Ok(()) => (),
+                Err(_msg) => return UnknownCpu::Inter(InterruptedCpu{cpu}),
+            }
+        }
+        UnknownCpu::Ok(cpu)
+    }
+
+    fn store_32_bo(mut cpu: Cpu) -> UnknownCpu {
+        let instruction = cpu.current_instruction();
+        let base = cpu.read(instruction.r_base());
+        let memory_address = base + instruction.i_offset() as u8;
+        println!("STORE|FROM:{}, TO:{}", instruction.r_target(), memory_address);
+        //TODO Make atomic
+        for i in 0..4 {
+            match cpu.copy_to_memory(instruction.r_target() + i, memory_address + i) {
+                Ok(()) => (),
+                Err(_msg) => return UnknownCpu::Inter(InterruptedCpu{cpu}),
+            }
         }
         UnknownCpu::Ok(cpu)
     }
@@ -779,6 +808,35 @@ mod tests {
 
                 let value = cpu.memory.read(address).unwrap();
                 assert_eq!(rand_value, value, "{}:{}|Inst:{}\nexp:{:X}\nval:{:X}", i, address, instruction, rand_value, value);
+            }
+        }
+    }
+
+    #[test]
+    fn test_st_16_bo_offset() {
+        let mut cpu = Cpu::new_blank();
+        let mut rng = rand::thread_rng();
+        for i in 0..10 { 
+            for address in 8..31 {
+                println!("{}", address);
+                let mut instruction = Instruction::from_opcode(25, 0);
+                instruction.r_target_set(5);
+                instruction.r_base_set(7);
+                instruction.i_offset_set(address as u32);
+                let rand_value: u16 = rng.gen();
+                cpu.write(5, (rand_value >> 8) as u8);
+                cpu.write(6, (rand_value & 0xFF) as u8);
+                cpu.write(7, 0);
+
+                cpu.load_instruction(1, &instruction);
+                cpu = match cpu.clock() {
+                    UnknownCpu::Ok(cpu) => cpu,
+                    UnknownCpu::Inter(cpu) => panic!("Unexpected intrrupt {}", cpu.release()),
+                };
+
+                let value = (cpu.memory.read(address).unwrap() as u16) << 8 |
+                    (cpu.memory.read(address + 1).unwrap()) as u16;
+                assert_eq!(rand_value, value as u16, "{}:{}|Inst:{}\nexp:{:X}\nval:{:X}", i, address, instruction, rand_value, value);
             }
         }
     }
